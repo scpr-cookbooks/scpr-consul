cedb87ff693e2ff93de16e343cecbe9573d60507
Eric Richardson <e@ericrichardson.com>
2014-11-20 17:20:43 -0500

    scpr-consul 0.1.5: Don't specify servers in default attributes
    * Removing these makes the defaults a little more test-kitchen friendly

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,5 +2,6 @@ default.scpr_consul.checks_dir = "/etc/consul_checks"
 
 include_attribute "consul"
 
-default.consul.servers  = ["consul1.i.scprdev.org","consul2.i.scprdev.org","consul3.i.scprdev.org"]
+#default.consul.servers  = ["consul1.i.scprdev.org","consul2.i.scprdev.org","consul3.i.scprdev.org"]
+default.consul.servers = ['127.0.0.1']
 default.consul.ui_dir   = "/usr/local/consul_ui"
