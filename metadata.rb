a47f2b9379ee5e47e119a9d1ce04566f478c51f0
Eric Richardson <e@ericrichardson.com>
2015-03-02 07:48:54 -0500

    0.1.21: Set consul cookbook version requirement to 0.8.3

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'erichardson@scpr.org'
 license          'all_rights'
 description      'Installs/Configures scpr-consul'
 long_description 'Installs/Configures scpr-consul'
-version          '0.1.20'
+version          '0.1.21'
 
-depends "consul"
+depends "consul", "~> 0.8.3"
 depends "dnsmasq"
