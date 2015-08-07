fa954c262d391bc8873ec9b0cf273ba26815e2bc
Eric Richardson <e@ericrichardson.com>
2015-08-07 10:11:05 -0400

    0.1.25 - Relax consul cookbook version constraint to < 1

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'erichardson@scpr.org'
 license          'all_rights'
 description      'Installs/Configures scpr-consul'
 long_description 'Installs/Configures scpr-consul'
-version          '0.1.24'
+version          '0.1.25'
 
-depends "consul", "~> 0.8.3"
+depends "consul", "~> 0.11"
 depends "dnsmasq"
