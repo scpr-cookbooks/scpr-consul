da9c13ef2c33982216addb4ee09cd1fccdc6e1e5
Eric Richardson <e@ericrichardson.com>
2016-02-02 12:21:39 -0500

    0.2.0: Add consul-template recipe

--- attributes/default.rb
+++ attributes/default.rb
@@ -29,3 +29,18 @@ default.consul.checksums['0.5.0_darwin_amd64'] = '24d9758c873e9124e0ce266f118078
 default.consul.checksums['0.5.0_linux_386']    = '4b6147c30596a30361d4753d409f8a1af9518f54f5ed473a4c4ac973738ac0fd',
 default.consul.checksums['0.5.0_linux_amd64']  = '161f2a8803e31550bd92a00e95a3a517aa949714c19d3124c46e56cfdc97b088',
 default.consul.checksums['0.5.0_web_ui']       = '0081d08be9c0b1172939e92af5a7cf9ba4f90e54fae24a353299503b24bb8be9'
+
+#----------
+
+include_attribute "consul-template"
+
+default.consul_template.base_url = "https://s3-us-west-2.amazonaws.com/scpr-binaries/consul-template/"
+default.consul_template.version = "0.12.2"
+default.consul_template.checksums = {
+  'consul-template_0.7.0_linux_amd64' => '7b8fb97caef72f9e67bbb9069042b8e01f7efed3acd2a32f560a8fe60146d874',
+  'consul-template_0.12.2_linux_amd64' => '525b7ea6a2912c67ab9fcebb603b60d9ab69ed572d6a753803f8c46badec10da',
+}
+
+default.consul_template.config = {
+  consul: "localhost:8500",
+}
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,8 @@ maintainer_email 'erichardson@scpr.org'
 license          'all_rights'
 description      'Installs/Configures scpr-consul'
 long_description 'Installs/Configures scpr-consul'
-version          '0.1.25'
+version          '0.2.0'
 
 depends "consul", "~> 0.11"
+depends "consul-template", "~> 0.8.0"
 depends "dnsmasq"
